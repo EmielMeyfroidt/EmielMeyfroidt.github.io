d03ec1800562b3718819a5d8b8ad2edc31f50e29
EmielMeyfroidt <48209313+EmielMeyfroidt@users.noreply.github.com>
2026-07-15 22:49:31 +0200

    Enhance image display with gallery and path refactor
    Refactored image path handling and added gallery display functionality.

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-// List of image file names 
-const images =  [
+const images = [
     'boom.jpg',
     'ijsbeer.jpg',
     'Jone_en_koe.jpg',
     'kameleon.jpg',
     'luipaard.jpg'
 ];
-// Function to select a random image 
-function displayRandomImage() { 
-    const randomIndex = Math.floor(Math.random() * images.length); 
-    const randomImage = images[randomIndex]; 
-    document.getElementById('random-image').src = `/pictures/${randomImage}`; 
-} 
-// Call the function to display a random image on page load 
-window.onload = displayRandomImage;
+
+const picturePath = 'pictures/';
+
+function displayRandomImage() {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex];
+
+    document.getElementById('random-image').src =
+        picturePath + randomImage;
+}
+
+function displayGallery() {
+    const gallery = document.getElementById('gallery');
+
+    images.forEach(image => {
+        const imageElement = document.createElement('img');
+
+        imageElement.src = picturePath + image;
+        imageElement.alt = 'Memory';
+
+        gallery.appendChild(imageElement);
+    });
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    displayRandomImage();
+    displayGallery();
+});
